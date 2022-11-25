96034077e9502b38b24e6d8d4dca591e74c9711c
Ilze Safi <ilze.safi2021@gmail.com>
2022-11-25 15:01:59 +0200

    Mary project

---
+++ Mary/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Mary's day");
+
+Console.WriteLine("What is the day today?");
+
+string answer = Console.ReadLine();
+
+
+if (answer == "working day")
+{
+    Console.WriteLine("Need to go to the work");
+}
+
+else if (answer == "holiday")
+{
+    Console.WriteLine("What is the weather?");
+
+    string weather = Console.ReadLine();
+
+    if (weather == "sun")
+    {
+        Console.WriteLine("Go to the beach!");
+    }
+
+    else if (weather == "rain")
+    {
+        Console.WriteLine("Go to the shopping center!");
+    }
+
+    else
+    {
+        Console.WriteLine("Stay at home!");
+    }
+}
+else
+
+{
+    Console.WriteLine("No match");
+}
+
+
+
